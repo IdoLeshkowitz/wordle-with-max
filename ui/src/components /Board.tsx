f779f9854f8e8639cdcf5453a5c13ad64e92d624
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-01-26 23:53:51 +0200

    css major changes

--- ui/src/components /Board.tsx
+++ ui/src/components /Board.tsx
@@ -1,25 +1,21 @@
 import {NonEvaluatedGuess} from "../../../commonTypes/NonEvaluatedGuess";
 import React from "react";
-const emptyGuess : NonEvaluatedGuess = {letter: '', index: 0}
-const Board = (props : any)=>{
-    const {numberOfTiles, guesses} = props
-    return (
-        <div className="board">
-            {Array(numberOfTiles).fill(0).map((_, index) => {
-                return <Tile key={index} guess={guesses[index] || emptyGuess} />
-            })}
-        </div>
-    )
-}
-const Tile = (props : any)=>{
-    const {guess} = props
-    console.log(guess)
-    return (
-        <div className="board-tile">
-            {guess.letter}
-        </div>
-    )
+import {EvaluatedGuess} from "../../../commonTypes/EvaluatedGuess";
+import Tile from "./Tile";
 
+const emptyGuess: NonEvaluatedGuess = {letter: '', index: 0}
+const Board = (props: any) => {
+    const {numberOfTiles, guesses} = props
+    return (<div className="board">
+        {Array(numberOfTiles).fill(0).map((_, index) => {
+            return <Tile
+                key={index}
+                guess={guesses[index] || emptyGuess}
+                isFocused={index === guesses.length }
+            />
+        })}
+    </div>)
 }
+
 
 export default Board
