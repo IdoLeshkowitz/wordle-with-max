080ea7c60aa897dbbdd501f623b981fbae52ceae
IdoLeshkowitz <idoleshkowitz@gmail.com>
2023-01-30 16:59:52 +0200

    signup

--- ui/src/components/modals/SignupModal.tsx
+++ ui/src/components/modals/SignupModal.tsx
@@ -17,6 +17,16 @@ const SignupModal = () => {
             onSubmit={handleSubmit}
             onClick={(e)=>{e.stopPropagation()}}
             className='d-flex flex-column align-items-center justify-content-center modal-container'>
+            <h1 className="text-light mb-4" >Sign-Up</h1>
+            <input className={'form-control mt-2'} name={'Name'} type={'text'}
+                   placeholder={'Enter your first name'} required/>
+            <input className={'form-control mt-2'} name={'Surname'} type={'text'}
+                   placeholder={'Enter your last name'} required/>
+            <input className={'form-control mt-2'} name={'Email'} type={'text'}
+                   placeholder={'Enter your Email'} required/>
+            <input className={'form-control mt-2'} name={'password'} type={'text'}
+                   placeholder={'Enter a new Password'} required/>
+            <button className={'mt-2 btn btn-outline-secondary'} type={'submit'}>Submit</button>
         </form>
     )
 }
