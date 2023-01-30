3fccbb38605deffc1eb88dd5992a8c98a1ddde90
IdoLeshkowitz <117535539+IdoLeshkowitz@users.noreply.github.com>
2023-01-30 17:03:53 +0200

    Delete ui/src/Pages directory

--- ui/src/Pages/Welcome.tsx
+++
@@ -1,9 +0,0 @@
-import {useNavigate} from "react-router-dom";
-import {useAppSelector} from "../redux/hooks";
-import {useEffect} from "react";
-
-export const Welcome = () => {
-    const navigate = useNavigate();
-    const {currentUser} = useAppSelector(state => state.user)
-    return <h1></h1>;
-};
