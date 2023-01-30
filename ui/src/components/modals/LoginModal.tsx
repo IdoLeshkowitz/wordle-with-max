5ad0152e00a21cd541194560327e0c959d35cca2
Max Elbirt <maxelbirt@gmail.com>
2023-01-30 18:18:34 +0200

    welcome page menu ready, fixed login modal a bit

--- ui/src/components/modals/LoginModal.tsx
+++ ui/src/components/modals/LoginModal.tsx
@@ -1,48 +1,44 @@
-import {useAppDispatch} from "../../redux/hooks";
-import React, {FormEvent, FormEventHandler} from "react";
-import GoogleConnect from "../GoogleConnect";
-import {login, LoginPayload} from "../../redux/Features/user/userActions";
-import {openModal} from "../../redux/Features/overlays/overlaysActions";
-import {ModalType} from "../../redux/Features/overlays/overlaysSlice";
+import { useAppDispatch } from '../../redux/hooks';
+import React, { FormEvent, FormEventHandler } from 'react';
+import GoogleConnect from '../GoogleConnect';
+import { login, LoginPayload } from '../../redux/Features/user/userActions';
+import { openModal } from '../../redux/Features/overlays/overlaysActions';
+import { ModalType } from '../../redux/Features/overlays/overlaysSlice';
 
 const LoginModal = () => {
     //no need for the onsubmit event, it's part of the form component
-    const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch();
     const handleSubmit: FormEventHandler = (event: FormEvent) => {
         event.preventDefault();
         const form = event.currentTarget as HTMLFormElement;
         const data = new FormData(form);
-        const {email, password} = Object.fromEntries(data.entries());
-        const loginPayload: LoginPayload = {email: email.toString(), password: password.toString()}
-        dispatch(login(loginPayload))
-    }
+        const { email, password } = Object.fromEntries(data.entries());
+        const loginPayload: LoginPayload = { email: email.toString(), password: password.toString() };
+        dispatch(login(loginPayload));
+    };
     const signUpClick = () => {
-        dispatch(openModal(ModalType.signup))
-    }
-
+        dispatch(openModal(ModalType.signup));
+    };
 
     return (
         <form
             onSubmit={handleSubmit}
             onClick={(e) => {
-                e.stopPropagation()
+                e.stopPropagation();
             }}
-            className='d-flex flex-column align-items-center align-content-between justify-content-center modal-container '>
-            <input className={'form-control mt-2'} name={'email'} type={'text'}
-                   placeholder={'Enter your email'}/>
-            <input className={'form-control mt-2'} name={'password'} type={'text'}
-                   placeholder={'Enter your password'}/>
-            <button className={'mt-2 btn btn-outline-secondary'} type={'submit'}>Submit</button>
-            <section className='d-flex justify-content-evenly '>
-                <GoogleConnect/>
-                <button
-                    className={'mt-2 btn btn-outline-secondary'}
-                    type='button'
-                    onClick={signUpClick}>
-                    sign up
+            className="d-flex flex-column align-items-center align-content-between justify-content-center modal-container "
+        >
+            <input className={'form-control mt-2'} name={'email'} type={'text'} placeholder={'Enter your email'} />
+            <input className={'form-control mt-2 mb-3'} name={'password'} type={'text'} placeholder={'Enter your password'} />
+            <div className="d-flex flex-row w-50 justify-content-md-center align-items-center">
+                <button className={'mt-2 btn btn-outline-secondary w-25'} type={'submit'}>
+                    Submit
                 </button>
-            </section>
+                <GoogleConnect />
+            </div>
+            <button className={'mt-2 btn btn-outline-secondary w-25'} type="button" onClick={signUpClick}>sign up
+            </button>
         </form>
-    )
-}
-export default LoginModal
+    );
+};
+export default LoginModal;
--- ui/src/pages/Welcome.tsx
+++ ui/src/pages/Welcome.tsx
@@ -5,5 +5,12 @@ import {useEffect} from "react";
 export const Welcome = () => {
     const navigate = useNavigate();
     const {currentUser} = useAppSelector(state => state.user)
-    return <h1></h1>;
+    return (
+        <div className="welcom_container border-1 h-100 m-auto d-flex flex-column align-items-center justify-content-center">
+            <h1 className="text-light mb-5 display-1 fw-bold">Wordle</h1>
+            <button className="btn btn-outline-secondary w-25">Play</button>
+            <button className="btn btn-outline-secondary w-25 mt-1">Log-In</button>
+            <button className="btn btn-outline-secondary w-25 mt-1">Sign-Up</button>
+        </div>
+    );
 };
